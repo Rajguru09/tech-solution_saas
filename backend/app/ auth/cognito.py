776f462e0777a20f50a2955393aee41058003fba
Rajguru09 <rajesh09guru@gmail.com>
2025-05-23 22:28:40 +0530

    Update cognito.py

--- backend/app/ auth/cognito.py
+++ backend/app/ auth/cognito.py
@@ -1,1 +1,45 @@
+import boto3
+from botocore.exceptions import ClientError
+from fastapi import HTTPException
+from app.utils.config import COGNITO_CLIENT_ID, COGNITO_REGION
 
+client = boto3.client("cognito-idp", region_name=COGNITO_REGION)
+
+def sign_up_user(username: str, password: str, email: str):
+    try:
+        response = client.sign_up(
+            ClientId=COGNITO_CLIENT_ID,
+            Username=username,
+            Password=password,
+            UserAttributes=[
+                {"Name": "email", "Value": email}
+            ]
+        )
+        return response
+    except ClientError as e:
+        raise HTTPException(status_code=400, detail=e.response['Error']['Message'])
+
+def confirm_sign_up(username: str, confirmation_code: str):
+    try:
+        response = client.confirm_sign_up(
+            ClientId=COGNITO_CLIENT_ID,
+            Username=username,
+            ConfirmationCode=confirmation_code,
+        )
+        return response
+    except ClientError as e:
+        raise HTTPException(status_code=400, detail=e.response['Error']['Message'])
+
+def login_user(username: str, password: str):
+    try:
+        response = client.initiate_auth(
+            ClientId=COGNITO_CLIENT_ID,
+            AuthFlow="USER_PASSWORD_AUTH",
+            AuthParameters={
+                "USERNAME": username,
+                "PASSWORD": password
+            }
+        )
+        return response['AuthenticationResult']
+    except ClientError as e:
+        raise HTTPException(status_code=400, detail=e.response['Error']['Message'])
